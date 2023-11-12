884d325124b190152df31e9072a27461c925ecb1
mrowaha <ashfaqrowaha@gmail.com>
2023-11-12 13:47:54 +0300

    upgrade java to 21

--- README.md
+++
@@ -1,1 +0,0 @@
-# CampusConnect API
---
+++ src/main/java/com/campusconnect/api/user/entity/User.java
@@ -0,0 +1,11 @@
+package com.campusconnect.api.user.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@MappedSuperclass
+public class User {
+}
