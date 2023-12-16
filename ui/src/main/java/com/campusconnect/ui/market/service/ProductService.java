b622b59f5c2d3b8fcf90a1b63df522755f942dde
İsmail Özgenç <ismailozgencofficial@gmail.com>
2023-12-16 10:54:58 +0300

    Product tags auto-assignment, product bids API

--- ui/src/main/java/com/campusconnect/ui/market/service/ProductService.java
+++ ui/src/main/java/com/campusconnect/ui/market/service/ProductService.java
@@ -3,7 +3,9 @@ package com.campusconnect.ui.market.service;
 import com.campusconnect.domain.ProductTag.entity.ProductTag;
 import com.campusconnect.domain.ProductTag.enums.ProductTagStatus;
 import com.campusconnect.domain.ProductTag.repository.ProductTagRepository;
+import com.campusconnect.domain.forumPost.entity.ForumPost;
 import com.campusconnect.domain.product.dto.ProductDto;
+import com.campusconnect.domain.product.dto.ProductSearchDto;
 import com.campusconnect.domain.product.entity.Product;
 import com.campusconnect.domain.product.enums.ProductStatus;
 import com.campusconnect.domain.product.repository.ProductRepository;
@@ -11,10 +13,17 @@ import com.campusconnect.domain.user.entity.Bilkenteer;
 import com.campusconnect.domain.user.repository.BilkenteerRepository;
 import com.campusconnect.domain.transaction.entity.Bid;
 import com.campusconnect.ui.market.exceptions.ProductNotFoundException;
-import com.campusconnect.ui.productTag.service.ProductTagService;
 import com.campusconnect.ui.user.exceptions.UserNotFoundException;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Predicate;
+import jakarta.persistence.criteria.Root;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -25,12 +34,16 @@ import java.util.stream.Collectors;
 
 @Service
 @Transactional
+@Slf4j
 @RequiredArgsConstructor
 public class ProductService {
+
     private final ProductRepository productRepository;
     private final ProductTagRepository productTagRepository;
     private final BilkenteerRepository bilkenteerRepository;
 
+    @Autowired
+    private EntityManager entityManager;
 
     public ResponseEntity<?> saveProduct(ProductDto productCreationInfo){
         Bilkenteer bilkenteer = bilkenteerRepository.findById(UUID.fromString(productCreationInfo.getSellerId()))
@@ -43,8 +56,7 @@ public class ProductService {
         }
 
         Product product = Product.builder()
-                .sellerId(UUID.fromString(productCreationInfo.getSellerId()))
-                .bilkenteer(bilkenteer)
+                .seller(bilkenteer)
                 .creationDate(LocalDate.now())
                 .name(productCreationInfo.getName())
                 .description(productCreationInfo.getDescription())
@@ -62,19 +74,13 @@ public class ProductService {
     }
 
     public List<Product> fetchProductList(){
-        List<Product> allProducts = (List<Product>) productRepository.findAll();
+        List<Product> allProducts = productRepository.findAll();
 
         List<Product> availableProducts = allProducts.stream()
                 .filter(product -> product.getStatus() == ProductStatus.AVAILABLE)
                 .collect(Collectors.toList());
 
         return availableProducts;
-    }
-
-    public List<Bid> getProductBids(UUID productId) {
-        Product product = productRepository.findById(productId).orElseThrow(() -> new ProductNotFoundException());
-
-        return product.getBids();
     }
 
     public Product updateProduct(ProductDto productCreationInfo, UUID productId){
@@ -90,6 +96,10 @@ public class ProductService {
 
         if (Objects.nonNull(productCreationInfo.getPrice())){
             productDB.setPrice(productCreationInfo.getPrice());
+        }
+
+        if (Objects.nonNull(productCreationInfo.getProductStatus())){
+            productDB.setStatus(productCreationInfo.getProductStatus());
         }
 
         return productRepository.save(productDB);
@@ -114,7 +124,68 @@ public class ProductService {
     }
 
     public List<Product> fetchProductsByUserId(UUID userId) throws UserNotFoundException {
-        Bilkenteer bilkenteer = bilkenteerRepository.findById(userId).orElseThrow(UserNotFoundException::new);
-        return bilkenteer.getProducts();
+        return productRepository.findAllBySellerUserId(userId).orElseThrow(UserNotFoundException::new);
     }
+
+    public List<Bid> getProductBids(UUID productId) {
+        Product product = productRepository.findById(productId).orElseThrow(() -> new ProductNotFoundException());
+
+        return product.getBids();
+    }
+
+    public List<Product> searchProduct(ProductSearchDto productSearchDto) {
+        CriteriaBuilder cb = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Product> cq = cb.createQuery(Product.class);
+
+        Root<Product> product = cq.from(Product.class);
+        List<Predicate> predicates = new ArrayList<>();
+
+        //Keyword can Match title or Description
+        if (productSearchDto.getKeywords() != null) {
+            String likePattern = "%" + productSearchDto.getKeywords().toLowerCase() + "%";
+            Predicate namePredicate = cb.like(cb.lower(product.get("name")), likePattern);
+            Predicate descriptionPredicate = cb.like(cb.lower(product.get("description")), likePattern);
+            predicates.add(cb.or(namePredicate, descriptionPredicate));
+        }
+
+        //Tags must be present
+        if (productSearchDto.getTags() != null && !productSearchDto.getTags().isEmpty()) {
+            predicates.add(product.join("tags").in(productSearchDto.getTags()));
+        }
+
+        if (productSearchDto.getMinPrice() != null) {
+            predicates.add(cb.greaterThanOrEqualTo(product.get("price"), productSearchDto.getMinPrice()));
+        }
+
+        if (productSearchDto.getMaxPrice() != null) {
+            predicates.add(cb.lessThanOrEqualTo(product.get("price"), productSearchDto.getMaxPrice()));
+        }
+
+        //Minimum Trust Score
+        if (productSearchDto.getSellerRating() != null) {
+            predicates.add(cb.greaterThanOrEqualTo(product.get("seller").get("trustScore"), productSearchDto.getSellerRating()));
+        }
+
+        cq.where(predicates.toArray(new Predicate[0]));
+
+        //Sorting Logic
+        if (productSearchDto.getSortBy() != null) {
+            switch (productSearchDto.getSortBy()) {
+                case PRICE_ASCENDING:
+                    cq.orderBy(cb.asc(product.get("price")));
+                    break;
+                case PRICE_DESCENDING:
+                    cq.orderBy(cb.desc(product.get("price")));
+                    break;
+                case LATEST:
+                    cq.orderBy(cb.desc(product.get("creationDate")));
+                    break;
+            }
+        }
+
+        //Run native query
+        TypedQuery<Product> query = entityManager.createQuery(cq);
+        return query.getResultList();
+    }
+
 }
