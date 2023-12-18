213f3cfafa8aefd91fc84a4672ddf1b5cd3da3b4
mrowaha <ashfaqrowaha@gmail.com>
2023-12-18 04:23:26 +0300

    [NOTE] enabled OTP Auth on account creation

--- domain/src/main/java/com/campusconnect/domain/user/dto/OTPRequestDto.java
+++ domain/src/main/java/com/campusconnect/domain/user/dto/OTPRequestDto.java
@@ -1,5 +1,8 @@
 package com.campusconnect.domain.user.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -10,5 +13,9 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class OTPRequestDto {
+    @NotBlank(message = "email field cannot be blank")
+    @NotNull(message = "email cannot be null")
+    @Pattern(regexp = "[A-Za-z0-9._]+@([a-z]+.)*bilkent.edu.tr", message = "email must be under the domain .bilkent.edu.tr and local part can only contain english alphabets, digits, dot or underscore")
+
     String email;
 }
